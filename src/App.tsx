7a5532f3a9e2ff02e87bff5ce750c74ac2593c3e
Alexander Galabov <galabovalexandar@gmail.com>
2023-10-08 10:32:44 +0300

    initial personal summary

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello, folks! 👋</h1>
+      <p>
+        My name is Alexandar Galabov and I am from Sofia, Bulgaria. A very
+        active and outgoing person with passion for most sports out there. I
+        also like experimenting with new technologies and building small
+        projects. Here's some information on the things I currently keep myself
+        busy with:
+      </p>
+      <ul>
+        <li>
+          🧑‍💻 Working full-time as a Senior Software Engineer at Astea Solutions.
+        </li>
+        <li>🤖 Working part-time as a Senior Software Engineer at Team-GPT.</li>
+        <li>💻 Main contributor of @asteasolutions/zod-to-openapi.</li>
+        <li>👨‍🏫 Mentoring in our company's internship programs.</li>
+        <li>
+          🎓 A Bachelor degree in Software Engineering at Sofia University.
+        </li>
+        <li>
+          🎓 A Master's degree in Technology entrepreneurship at Sofia
+          University.
+        </li>
+        🕺 Attending Bachata & Salsa courses
+        <li>🏓 Professional table tennis player.</li>
+        <li>
+          🎯 2023 goal: Contribute more to Personal and Open Source projects.
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
